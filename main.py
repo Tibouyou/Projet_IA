057aac2bb0550259ab77f5f4d88ea4d48fc5f455
TaTu <arthur.rigonnet@gmail.com>
2023-05-03 20:11:45 +0200

    argument au lancement dans le cmd

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
+import argparse
 from connect4 import Connect4Game, Connect4Viewer, Connect4Console
 from mcts import MonteCarlo
 from const import *
 
-def main():
-    
-    #choix si interface console ou graphique
-    choix = int(input("Voulez-vous jouer en mode console ou graphique ? (1/2) : "))
-    game = Connect4Game("minimax","alphabeta")
-    if choix == 2:
+def main(interface, p1, p2, hide_board):
+    game = Connect4Game(p1,p2)
+    if interface == "graphique":
         viewer = Connect4Viewer(game)
     else:
-        console = Connect4Console(game , show_board = False)
+        console = Connect4Console(game , hide_board)
         console.play()
         
-
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--interface", help="Interface à utiliser : console ou graphique", default="console")
+    parser.add_argument("--p1", help="player 1 : minimax / alphabeta / mcts / human", default="minimax")
+    parser.add_argument("--p2", help="player 2 : minimax / alphabeta / mcts / human", default="alphabeta")
+    parser.add_argument("--hide_board", help="Afficher le plateau de jeu", action="store_false")
 
-    """player1 = "human"
-    player2 = "mcts"
-    game = Connect4Game(player1, player2)                     alphabeta ou minimax
-    console = Connect4Console(game)"""
+    args = parser.parse_args()
+    
+    main(args.interface, args.p1, args.p2, args.hide_board)
+
