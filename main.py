22e8daa93b6addc53989001e8e6db5872f3d2bb1
TaTu <arthur.rigonnet@gmail.com>
2023-05-03 21:10:04 +0200

    changes loading bar

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from connect4 import Connect4Game, Connect4Viewer, Connect4Console
-from mcts import MonteCarlo
 from const import *
 from tqdm import tqdm
 
@@ -21,24 +20,23 @@ def play_games(args):
     # initialiser les résultats
     results = {'player_1': 0, 'player_2': 0}
 
-    # créer une instance de ThreadPoolExecutor avec 6 threads
+    # créer une instance de ThreadPoolExecutor avec args.max_workers threads
     with ThreadPoolExecutor(max_workers=args.max_workers) as executor:
         # lancer plusieurs parties en parallèle
         futures = []
         for i in range(args.num_games):
             future = executor.submit(play_game, args.interface, args.p1, args.p2, args.hide_board, results)
             futures.append(future)
-
+        
         # créer une barre de progression
         with tqdm(total=args.num_games) as pbar:
             # attendre que toutes les parties soient terminées
-            while len(futures) > 0:
+            for future in as_completed(futures):
                 # mettre à jour la barre de progression
-                pbar.update(args.num_games - len(futures))
-                # supprimer les futures terminés de la liste
-                for future in futures.copy():
-                    if future.done():
-                        futures.remove(future)
+                pbar.update(1)
+        
+        # attendre la fin de toutes les tâches
+        executor.shutdown()
 
     # afficher les résultats
     print(f"Nombre de victoires du joueur 1 ({args.p1}) : {results['player_1']} Win rate : {results['player_1']/args.num_games*100}%")
